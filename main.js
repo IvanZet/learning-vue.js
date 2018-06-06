075c007a03da0532eb9172b3b3026563c6174802
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-06-06 23:19:30 +0200

    Lesson 2: adds binding attributes of imgs and links via v-bind

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-var product = "Socks";
 var app = new Vue ({
 	el: '#app',
 	data: {
 		product: "Socks",
-		description: "A pair of warm, fuzzy socks"
+		description: "A pair of warm, fuzzy socks",
+		image: "./assets/vmSocks-green-onWhite.jpg",
+		link: "https://www.kommersant.ru/"
 	}
 });
